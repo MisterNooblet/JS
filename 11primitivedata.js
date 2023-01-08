b3ec2f9245e849507028850136d93da135987a56
misternooblet <dragonroadsupport@gmail.com>
2023-01-08 10:18:03 +0200

    11

---
+++ 11primitivedata.js
@@ -0,0 +1,15 @@
+var string1 = "Hello world";
+var number1 = 555;
+var object1 = [];
+var boolean1 = true;
+var varnull1 = null;
+var undefined1 = undefined;
+var symbol1 = Symbol();
+
+console.log(typeof string1);
+console.log(typeof number1);
+console.log(typeof object1);
+console.log(typeof boolean1);
+console.log(typeof varnull1);
+console.log(typeof undefined1);
+console.log(typeof symbol1);
