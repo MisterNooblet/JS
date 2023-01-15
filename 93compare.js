b5c850e9ad6c204af03040203c16033fdffad81f
misternooblet <dragonroadsupport@gmail.com>
2023-01-15 11:31:08 +0200

    compare

--- 93compare.js
+++ 93compare.js
@@ -3,8 +3,6 @@ const food1 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
 compare(food, food1);
 function compare(arr1, arr2) {
   for (const i in arr1) {
-    let includes1 = null;
-    let includes2 = null;
     for (const x in arr2) {
       if (arr2[x] === arr1[i]) {
         console.log(arr1[i]);
