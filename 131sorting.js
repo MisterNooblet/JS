a2c22db5757dbf509032eb888e3908ae80489dd9
misternooblet <dragonroadsupport@gmail.com>
2023-01-17 11:35:15 +0200

    sort

---
+++ 131sorting.js
@@ -0,0 +1,6 @@
+const numbers = [1, -5, 666, 2, 400, 11];
+
+const asc = numbers.sort((a, b) => a - b);
+console.log(asc);
+const desc = numbers.sort((a, b) => b - a);
+console.log(desc);
---
+++ 132sortingstrings.js
@@ -0,0 +1,61 @@
+const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
+
+const asc = foods.sort();
+console.log(asc);
+
+const desc = foods.sort((a, b) => {
+  if (a < b) {
+    return 1;
+  }
+  if (a > b) {
+    return -1;
+  }
+  return 0;
+});
+console.log(desc);
+
+const foodsWithUpperCase = [
+  "falafel",
+  "Sabich",
+  "hummus",
+  "pizza with extra pineapple",
+];
+
+const withUpperCaseAsc = foodsWithUpperCase.sort((a, b) => {
+  const aLower = a.toLowerCase();
+  const bLower = b.toLowerCase();
+
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+});
+console.log(withUpperCaseAsc);
+
+const withUpperCaseDesc = foodsWithUpperCase.sort((a, b) => {
+  const aLower = a.toLowerCase();
+  const bLower = b.toLowerCase();
+
+  if (a < b) {
+    return 1;
+  } else if (a > b) {
+    return -1;
+  }
+  return 0;
+});
+console.log(withUpperCaseDesc);
+
+const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
+
+const shortLong = words.sort((a, b) => {
+  if (a.length < b.length) {
+    return 1;
+  } else if (a.length > b.length) {
+    return -1;
+  }
+  return 0;
+});
+
+console.log(shortLong);
