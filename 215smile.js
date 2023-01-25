e6ccbd90e081c31c812dcf9fee7ce36ec5a9bb78
misternooblet <dragonroadsupport@gmail.com>
2023-01-25 14:19:52 +0200

    22

--- 215smile.js
+++ 215smile.js
@@ -1,34 +1,32 @@
 const field = document.querySelector('textarea');
 const button = document.querySelector('button');
 const body = document.querySelector('body');
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     if (!isNaN(field.value * 1)) {
         if (document.querySelector('.smileybox') !== null) {
             document.querySelector('.smileybox').remove()
         }
-        makeItRain(field.value*1)
-    }else{
+        makeItRain(field.value * 1)
+    } else {
         alert('Only numbers allowed!')
     }
 })
 
 function makeItRain(amm) {
-   const box = document.createElement('div');
-   
-
-   box.classList.add('smileybox');
+    const box = document.createElement('div');
+    box.classList.add('smileybox');
     body.appendChild(box)
 
     for (let i = 0; i < amm; i++) {
-        if(i%2 == 0){
+        if (i % 2 == 0) {
             const angry = document.createElement('img');
-   angry.src = 'https://cdn-icons-png.flaticon.com/128/743/743418.png'
+            angry.src = 'https://cdn-icons-png.flaticon.com/128/743/743418.png'
             box.appendChild(angry)
-        }else{
+        } else {
             const happy = document.createElement('img');
             happy.src = 'https://cdn-icons-png.flaticon.com/128/743/743267.png'
             box.appendChild(happy)
         }
-        
+
     }
 }
